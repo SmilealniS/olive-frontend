2276bc9727420e18ccd8b17e1c2d0bf8d613d0e5
SmilealniS <tonnam9639@gmail.com>
2023-01-17 00:19:38 +0700

    Get Zoom start with React

--- src/index.js
+++ src/index.js
@@ -4,11 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Meeting from './meeting';
+
+let payload = {
+  meetingNumber: "4318372796",
+  role: 0,
+  sdkKey: "6V8X5gwmS7lhH6EcVpCPXY0bBduD7Vnwx4QV",
+  sdkSecret: "XRSfgcqn75DdVZ0P3Nkf0WXZQdsonas5I6nV",
+  userName: "NamTest",
+  userEmail: "",
+  passWord: "180HYZ",
+  leaveUrl: "https://localhost:3000"
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/meeting',
+    element: <Meeting payload = {payload}/>
+  }
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router = {router}></RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/meeting.js
@@ -0,0 +1,56 @@
+import { ZoomMtg } from "@zoomus/websdk";
+import React,{useEffect, Fragment} from "react";
+
+const Meeting = ({payload}) => {
+    useEffect(async ()=> {
+        const {ZoomMtg} = await import ("@zoomus/websdk")
+
+        ZoomMtg.setZoomJSLib("https://source.zoom.us/lib", "/av");
+
+        ZoomMtg.preLoadWasm();
+        ZoomMtg.prepareWebSDK();
+
+        ZoomMtg.generateSDKSignature({
+            meetingNumber: payload.meetingNumber,
+            role: payload.role,
+            sdkKey: payload.sdkKey,
+            sdkSecret: payload.sdkSecret,
+            success: function(signature) {
+                ZoomMtg.init({
+                    leaveUrl: payload.leaveUrl,
+                    success: function(data) {
+                        ZoomMtg.join({
+                            meetingNumber: payload.meetingNumber,
+                            signature: payload.signature,
+                            sdkKey: payload.sdkKey,
+                            userName: payload.userName,
+                            userEmail: payload.userEmail,
+                            passWord: payload.passWord,
+                            tk: '',
+                            success: function() {
+                                console.log('Successfully joined');
+                            },
+                            error: function(error) {
+                                console.log(error);
+                            }
+                        })
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                })
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+
+    }, [])
+
+    return (<Fragment>
+        <link type="text/css" rel="stylesheet" href="https://source.zoom.us/2.9.5/css/bootstrap.css" />
+        <link type="text/css" rel="stylesheet" href="https://source.zoom.us/2.9.5/css/react-select.css" />
+    </Fragment>);
+}
+
+export default Meeting;
