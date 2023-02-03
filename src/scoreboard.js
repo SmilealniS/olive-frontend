6e8f8f168f8d60f58b2e3b06598952a4f1d19c09
Kittitad Jiraprasitchai <52563334+maxkittitad@users.noreply.github.com>
2023-02-03 23:34:47 +0700

    Icon and chat text box
    Resize chat and problem at icon not show

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,7 +1,7 @@
 import './scoreboard.css';
 import React from 'react';
 
-const board=()=>{
+const board = () => {
 
   function chat() {
     alert('Hi');
@@ -11,40 +11,89 @@ const board=()=>{
     <body>
       <div class="grid-container">
         <div class="grid-item" id='display'>
-          <div class="grid-display">Display</div>
+          <div class='grid-item' id='r-tools'>Stats
+            <div class='grid-item' id=''>Statsss</div>
+            <div class='grid-item' id=''>Statsss</div>
+            <div class='grid-item' id='r-tools'>Statsss</div>
+          </div>
         </div>
-
-        {/* <div class="grid-item" id='bottom'>
-          <div class="grid-bottom">
-              <button onClick={''}>Leave</button>
-          </div>
-        </div> */}
-
-        {/* <div class="grid-item" id='chat'> 
-          <div class="grid-chat">
-          <button onClick={chat}>say hi</button>
-          </div>
-        </div> */}
       </div>
 
       <div class='grid-container' id='below'>
         <div class='grid-item'>
           <div class='grid-container' id='tools'>
             <div class='grid-item' id='l-tools'>Nav</div>
-            <div class='grid-item' id='r-tools'>Stats</div>
+            <div class='grid-item' id='r-tools'>Stats
+              <div class='grid-item' id='r-tools'>Statsss</div>
+            </div>
           </div>
-          <div class='grid-item' id='bottom'>Leave</div>
+          
+          <div class='grid-item' id='bottom'>
+            <button>
+              <i class="fa fa-microphone" area-hidden="true"></i>
+            </button>
+            <button>
+              <i class="fa fa-screen"></i>
+            </button>
+            <button>
+              <i class="fa fa-camera"></i>
+            </button>
+            <button>
+              Leave
+            </button>
+          </div>
         </div>
 
-        <div class='grid-item' id='chat'>Chat</div>
-      </div>
-      
-    </body>
-    );
-  }
+        <div class='grid-item' id='chat'>Chat
 
-  
-   
-  export default (board);
+          <div id="container">
+            <main>
+              <ul id="chat">
+                <li class="you">
+                  <div class="entete">
+                    <h2>mnct</h2>
+                    <h3>10:12AM, Today</h3>
+                  </div>
+                  <div class="message">
+                    Typing for test text
+                  </div>
+                </li>
+
+                <li class="me">
+                  <div class="entete">
+                    <h3>10:12AM, Today</h3>
+                    <h2>mnct</h2>
+                    <span class="status blue"></span>
+                  </div>
+                  <div class="message">
+                    OK
+                  </div>
+                </li>
+              </ul>
+              <footer>
+                <textarea placeholder="Type your message"></textarea>
+                <a href="#">Send</a>
+              </footer>
+            </main>
+          </div><br></br>
+          <button>
+            <i class="fa fa-user" area-hidden="true"></i>
+          </button>
+          <button>
+            <i class="fa fa-commenting-o" area-hidden="true"></i>
+          </button>
+          <button>
+            <i class="fa fa-smile-o" area-hidden="true"></i>
+          </button>
+        </div>
+      </div>
+
+    </body>
+  );
+}
+
+
+
+export default (board);
 
 
