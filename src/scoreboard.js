3b125b6ab552ed1b9ebc8827006706877743c224
Kittitad Jiraprasitchai <52563334+maxkittitad@users.noreply.github.com>
2023-02-04 21:52:04 +0700

    Update add scoreboard
    help fix css pls

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,20 +1,87 @@
 import './scoreboard.css';
 import React from 'react';
 
-const board = () => {
 
+const board = () => {
   function chat() {
     alert('Hi');
   }
 
   return (
+
     <body>
       <div class="grid-container">
         <div class="grid-item" id='display'>
-          <div class='grid-item' id='r-tools'>Stats
-            <div class='grid-item' id=''>Statsss</div>
-            <div class='grid-item' id=''>Statsss</div>
-            <div class='grid-item' id='r-tools'>Statsss</div>
+          <div class='grid-item' id='r-tools' >Leaderboard
+            <div class="center">
+              <div class="top3">
+                <div class="two item">
+                  <div class="pos">
+                    2
+                  </div>
+                  <div class="pic" style={{}}></div>
+                  <div class="name">
+                    User top2
+                  </div>
+                  <div class="score">
+                    10
+                  </div>
+                </div>
+                <div class="one item">
+                  <div class="pos">
+                    1
+                  </div>
+                  <div class="pic" style={{}}></div>
+                  <div class="name">
+                    User Top1
+                  </div>
+                  <div class="score">
+                    15
+                  </div>
+                </div>
+                <div class="three item">
+                  <div class="pos">
+                    3
+                  </div>
+                  <div class="pic" style={{}}></div>
+                  <div class="name">
+                    User top3
+                  </div>
+                  <div class="score">
+                    5
+                  </div>
+                </div>
+              </div>
+              <div class="list">
+                <div class="item">
+                  <div class="pos">
+                    4
+                  </div>
+                  <div class="pic" style={{}}></div>
+                  <div class="name">
+                    User top4
+                  </div>
+                  <div class="score">
+                    3
+                  </div>
+                </div>
+              </div>
+              <div class="list">
+                <div class="item">
+                  <div class="pos">
+                    5
+                  </div>
+                  <div class="pic" style={{}}></div>
+                  <div class="name">
+                    User top5
+                  </div>
+                  <div class="score">
+                    1
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
@@ -27,7 +94,7 @@ const board = () => {
               <div class='grid-item' id='r-tools'>Statsss</div>
             </div>
           </div>
-          
+
           <div class='grid-item' id='bottom'>
             <button>
               <i class="fa fa-microphone" area-hidden="true"></i>
